4d1331f89af46cba5f810b63c1d99b0ee755c3c0
mohaa02 <moha.essalhi11302@gmail.com>
2022-06-14 15:35:46 +0200

    4

--- Programacion1/Laboratorio/Laboratorio/Solucion.java
+++ Programacion1/Laboratorio/Laboratorio/Solucion.java
@@ -6,8 +6,7 @@ public class Solucion {
 	final static double DESCUENTO = 0.05;
 	final static String[] MENU = { "Salir", "Venta de billetes", "Consulta de asientos libres",
 	"Anulacion de billetes" };
-	//Hola 8
-	//Hola 6
+	
 	final static Scanner TECLADO = new Scanner(System.in);
 
 	public static void main(String[] args) {
